64a591440e1c66bdb5477103970dfb46b5513c48
3lown4way <jhkang060608@gmail.com>
2026-08-06 20:33:31 +0900

    Export virtual 3x3 reduction state

--- solver444-wasm/src/lib.rs
+++ solver444-wasm/src/lib.rs
@@ -5,6 +5,7 @@ mod edges;
 mod geometry;
 mod moves;
 mod parser;
+mod reduction;
 mod state;
 
 pub use api::{solve_444_boundary, solve_444_json, solver_444_api_version};
@@ -12,4 +13,7 @@ pub use centers::{solve_centers, CenterSolveError, CenterSolveResult};
 pub use edges::{solve_edges, EdgeSolveError, EdgeSolveResult};
 pub use moves::{Face, Move444, MoveParseError};
 pub use parser::{parse_alg444, AlgParseError};
+pub use reduction::{
+    normalize_parity, ParityNormalizeResult, ParitySignature, ReductionError, Virtual333State,
+};
 pub use state::{Cube444, StateValidationError, FACELET_COUNT};
