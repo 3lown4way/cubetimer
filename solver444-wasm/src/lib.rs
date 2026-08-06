096952aaee90a77352af8b2d66af45cb9a98619e
3lown4way <jhkang060608@gmail.com>
2026-08-06 16:59:03 +0900

    Export 4x4 WASM boundary

--- solver444-wasm/src/lib.rs
+++ solver444-wasm/src/lib.rs
@@ -1,8 +1,10 @@
+mod api;
 mod geometry;
 mod moves;
 mod parser;
 mod state;
 
+pub use api::{solve_444_boundary, solve_444_json, solver_444_api_version};
 pub use moves::{Face, Move444, MoveParseError};
 pub use parser::{parse_alg444, AlgParseError};
 pub use state::{Cube444, StateValidationError, FACELET_COUNT};
