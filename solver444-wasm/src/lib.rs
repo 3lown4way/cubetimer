230408d90fa5217cc84f5fc493dce03f1baa4d90
3lown4way <jhkang060608@gmail.com>
2026-08-06 18:28:49 +0900

    Expose 4x4 center solver

--- solver444-wasm/src/lib.rs
+++ solver444-wasm/src/lib.rs
@@ -1,10 +1,12 @@
 mod api;
+mod centers;
 mod geometry;
 mod moves;
 mod parser;
 mod state;
 
 pub use api::{solve_444_boundary, solve_444_json, solver_444_api_version};
+pub use centers::{solve_centers, CenterSolveError, CenterSolveResult};
 pub use moves::{Face, Move444, MoveParseError};
 pub use parser::{parse_alg444, AlgParseError};
 pub use state::{Cube444, StateValidationError, FACELET_COUNT};
